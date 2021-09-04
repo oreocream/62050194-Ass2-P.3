c36bd4d7619b00a5e8ded08e84ab4483e2e1bf65
oreocream <ozonezanaruk@gmail.com>
2021-09-04 18:19:53 +0700

    62050194 Ass2 P.3
    แก้ไข

--- Score/Main.java
+++ Score/Main.java
@@ -5,14 +5,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ScoreBoard obj1 = new ScoreBoard();
+        ScoreBoard1 obj1 = new ScoreBoard1();
+        ScoreBoard2 obj2 = new ScoreBoard2();
         HeadQuater obj = new HeadQuater();              
-        obj.register(obj1);             
+        obj.register(obj1); 
+        obj.register(obj2);                         
         while(true){
+            System.out.print("Enter Score :");
             String str = input.nextLine();
             if(!str.isEmpty()){
                 obj.setscore(str);
             }else return;
-        }    
+        } 
+    
     }
 }
